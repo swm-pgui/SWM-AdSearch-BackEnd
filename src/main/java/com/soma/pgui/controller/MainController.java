b3b4673e0aefc23a2f65c8c7c90a5e0f6ee59347
bontana <bg1346@naver.com>
2021-05-13 17:30:29 +0900

    add Entity PRODUCTS and add productsRepository
    add Entity "products"
    add EntityRepository "productsRepository"
    add dto "productsSaveRequestDto"
    add controller "productsController"
    add service "ProductsService"

--- src/main/java/com/soma/pgui/controller/MainController.java
+++ src/main/java/com/soma/pgui/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.soma.pgui.controller;
 
+import com.soma.pgui.domain.products.Products;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
+
+
+
 }
---
+++ src/main/java/com/soma/pgui/controller/ProductsController.java
@@ -0,0 +1,24 @@
+package com.soma.pgui.controller;
+
+import com.soma.pgui.domain.products.Products;
+import com.soma.pgui.dto.products.ProductsSaveRequestDto;
+import com.soma.pgui.service.ProductsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductsController {
+    private final ProductsService productsService;
+
+    @GetMapping("/bg1")
+    public Products bg1(){
+        ProductsSaveRequestDto productsSaveRequestDto = ProductsSaveRequestDto.builder().
+                name("name1")
+                .company("company1")
+                .build();
+
+        return productsSaveRequestDto.toEntity();
+    }
+}
