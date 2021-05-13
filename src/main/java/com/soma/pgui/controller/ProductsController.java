094cb40a5e80f3e69cc2482f3e22ecddd2b2439f
GuruneLee <dlckdgk4858@gmail.com>
2021-05-13 21:09:38 +0900

    GET /products/{id}

--- src/main/java/com/soma/pgui/controller/ProductsController.java
+++ src/main/java/com/soma/pgui/controller/ProductsController.java
@@ -1,10 +1,12 @@
 package com.soma.pgui.controller;
 
 import com.soma.pgui.domain.products.Products;
+import com.soma.pgui.dto.products.ProductsResponseDto;
 import com.soma.pgui.dto.products.ProductsSaveRequestDto;
 import com.soma.pgui.service.ProductsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +22,10 @@ public class ProductsController {
                 .build();
 
         return productsSaveRequestDto.toEntity();
+    }
+
+    @GetMapping("/products/{id}")
+    public ProductsResponseDto findById(@RequestParam Long id) {
+        return productsService.findById(id);
     }
 }
