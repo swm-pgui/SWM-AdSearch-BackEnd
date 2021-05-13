63ff2a563dc2103baaf47ff6c2f1165d219c6cca
bontana <bg1346@naver.com>
2021-05-13 14:20:46 +0900

    initial commit

---
+++ src/main/java/com/soma/pgui/PguiApplication.java
@@ -0,0 +1,13 @@
+package com.soma.pgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PguiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/soma/pgui/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.soma.pgui.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
